f5a3f5d20e8bfaba048bf84d594b5726ef680f1d
kanch08 <31513134+kanch08@users.noreply.github.com>
2024-09-23 10:50:21 +0530

    Delete shopping-cart directory

--- shopping-cart/frontend/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { RouterProvider } from "react-router-dom";
-import routes from "./routes/Router";
-
-const App = () => {
-  return <RouterProvider router={routes} />;
-};
-
-export default App;
--- shopping-cart/frontend/utils/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const BASE_URL = import.meta.env.VITE_BASE_URL;
-
-const API = axios.create({ baseURL: BASE_URL, withCredentials: true });
-
-export default API;
